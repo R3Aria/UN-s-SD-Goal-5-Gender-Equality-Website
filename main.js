33ebf876170333a491e3b634b0435e45b7870b66
Nanosync <r3.aria@gmail.com>
2025-09-16 15:18:47 -0400

    Almost done the Navigation Bar

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+// Just redirets the user when theu click the button
 function learnMore(){
     URL = "Other-Pages/Challenges.html"
     window.open(URL, "_self");
 }
 
+// For the Navigation bar animation
 function sideMenu(x){
+    // Toggles the animation
     x.classList.toggle("change");
 }
