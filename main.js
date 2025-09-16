74632400e2eb63fcdba5958a478df2914f5f12a4
Nanosync <r3.aria@gmail.com>
2025-09-16 16:12:55 -0400

    Added the close nav bar, commiting this before I mess something up again

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ function learnMore(){
 
 // For the Navigation bar animation
 function sideMenu(x){
+    const menu = document.querySelectorAll(".Menu-Icon");
+
     // Toggles the animation
     x.classList.toggle("change");
+}
+// SideNav
+function closeNav(){
+    let sideBar = document.getElementById("NavContent");
+    sideBar.style.width = 0;
+}
+function openNav(){
+    let sideBar = document.getElementById("NavContent");
+    sideBar.style.width = "100%";
 }
